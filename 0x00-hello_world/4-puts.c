1020c50d4ffad2951174b0f6f0961e30e7f998ff
yara <yaraahmed01@icloud.com>
2023-03-16 12:12:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main - a program that prints a line with puts
+ * return 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
