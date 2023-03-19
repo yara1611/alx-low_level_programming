b3278d76bab4494b2df25269e15030c6ff582d3e
yara <yaraahmed01@icloud.com>
2023-03-19 10:20:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+*main a program that prints the alphabet in lower case followed by
+*a new line.
+*Return: 0 (success)
+*/
+int main(void)
+{
+char x = a;
+while ( a < 'z' )
+{
+putchar(x);
+putchar('\n');
+a++;
+}
+returns (0);
+}
